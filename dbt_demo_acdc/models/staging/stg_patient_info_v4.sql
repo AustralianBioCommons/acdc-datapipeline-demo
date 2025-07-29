49c30a40df4561911a13c070ad63116113f31547
JoshuaHarris391 <harjo391@gmail.com>
2025-07-29 15:25:04 +1000

    current version

---
+++ dbt_demo_acdc/models/staging/stg_patient_info_v4.sql
@@ -0,0 +1,31 @@
+-- This model is materialized as a physical table by default
+-- It writes new Parquet files to your clean S3 bucket
+
+{{ config(materialized='table') }}
+
+-- This part *reads* data from an external source.
+with source as (
+
+    select * from {{ source('glue_db_acdc_dbt_test_raw', 'patient_info') }}
+
+),
+
+-- This part *transforms* the data that was read.
+cleaned as (
+
+    select
+        patient_id,
+        CASE
+            WHEN UPPER(TRIM(sex)) IN ('M', 'MALE', '0', 'Male') THEN 'male'
+            WHEN UPPER(TRIM(sex)) IN ('F', 'FEMALE', '1', 'fem', 'Female') THEN 'female'
+            ELSE NULL
+        END AS biological_sex_v2
+    from source
+    where patient_id <> 'patient_id'
+        and sex <> 'sex'
+        and patient_id is not NULL
+        and patient_id <> ''
+)
+
+-- This selects the final, cleaned data to be saved.
+select * from cleaned
