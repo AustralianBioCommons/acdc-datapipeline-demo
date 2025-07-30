906a0edbfae9196e3349b37717abb5e736004e52
JoshuaHarris391 <harjo391@gmail.com>
2025-07-30 10:24:44 +1000

    fixed double width declaration

--- dbt_demo_acdc/models/intermediate/int_sample_storage_v2.sql
+++ dbt_demo_acdc/models/intermediate/int_sample_storage_v2.sql
@@ -3,18 +3,11 @@
 
 {{ config(materialized='view') }}
 
--- This part *reads* data from an external source.
-with source as (
 
-    select * from {{ source('glue_db_acdc_dbt_test_raw', 'patient_info') }}
-
-),
-
-cleaned as (
+WITH cleaned as (
 
     select
         sample_id,
-        -- Standardize storage_medium to uppercase AIR/NITROGEN or NULL
         CASE
             WHEN UPPER(TRIM(storage_medium)) = 'AIR' THEN 'AIR'
             WHEN UPPER(TRIM(storage_medium)) = 'NITROGEN' THEN 'NITROGEN'
@@ -24,6 +17,4 @@ cleaned as (
         '{{ var("release_version") }}' as release_version
     from {{ ref('stg_sample_raw') }}
 )
-
--- This selects the final, cleaned data to be saved.
 select * from cleaned
